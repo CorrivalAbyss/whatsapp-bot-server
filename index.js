0e91878490c020524f2c2c0d0433850ec8517d00
CorrivalAbyss <145371170+CorrivalAbyss@users.noreply.github.com>
2026-03-06 19:21:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ app.use(express.json());
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        headless: false
+        headless: true,
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-dev-shm-usage",
+            "--single-process"
+        ]
     }
 });
 
